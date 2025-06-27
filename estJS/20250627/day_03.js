e8f088763d916eb7091c0cfefd53b0c72c5f420b
sc.lee <sclee9040@gmail.com>
2025-06-27 16:11:59 +0900

    조건식 비교 연산 자리 변경

--- estJS/20250627/day_03.js
+++ estJS/20250627/day_03.js
@@ -1,7 +1,5 @@
 const height = [150, 169, 185, 170];
 const ghost = 167;
-
-console.log(height.filter((x) => ghost < x).length);
-
+console.log(height.filter((x) => x > ghost).length);
 const words = ["hello", "ghost", "apple", "grape", "Lemon"];
 console.log(words.filter((x) => x.includes("l") || x.includes("L")));

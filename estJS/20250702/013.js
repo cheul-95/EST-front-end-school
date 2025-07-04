26b7bd09abb5a0fa40c6fa10066a04063c94e648
sc.lee <sclee9040@gmail.com>
2025-07-04 17:38:28 +0900

    클래스 생성함수 setInterval setTimeout

--- estJS/20250702/013.js
+++ estJS/20250702/013.js
@@ -10,3 +10,11 @@ const getBigNumber = (numbers) => {
     return Math.max(...numbers);
 };
 console.log(getBigNumber([3, 4, 10, 12]));
+
+const btn = document.getElementById("btn");
+btn.addEventListener("mouseleave", (x) => {
+    btn.textContent = "마우스를 올려보세요";
+});
+btn.addEventListener("mouseenter", (x) => {
+    btn.textContent = "마우스 IN";
+});
--- estJS/20250702/015.js
+++ estJS/20250702/015.js
@@ -1,6 +1,6 @@
 import { user1, user2 } from "../data/user.js";
 
-const { age, name, skills = "NO skills" } = user1;
-console.log(age, name, skills);
+const { age, name, skills = "NO skills", personal } = user1;
+console.log(age, name, skills, personal);
 const { age: newAge, name: newName, skills: newSkills = "NO skills" } = user2;
-console.log(newAge, newName, newSkills);
+console.log(newAge, newName, newSkills, personal);
